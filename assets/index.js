95dcd7da7610bf8057df6b3ebdfe3e55cc14f0e4
Nick Rolls <63081873+nrolls@users.noreply.github.com>
2024-07-31 14:40:31 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,14 @@
 const url = new URL(location.href)
 console.log(url)
-const roomUrl = new URL("https://funtimes.whereby.com")
+const roomUrl = new URL("https://nickrolls.whereby.com")
 roomUrl.pathname = url.pathname
 roomUrl.search = url.search
 
 const root = document.getElementById("root")
 const whereby = document.createElement("whereby-embed")
 whereby.setAttribute("room", roomUrl.href)
-whereby.setAttribute("minimal", true)
+whereby.setAttribute("floatSelf", "on")
+whereby.setAttribute("subgridLabels", "on")
+whereby.setAttribute("floatSelf", "on")
 whereby.setAttribute("style", "height: 100%;")
 root.appendChild(whereby)
